ae17da48b9c69adaddae2feb9bf22e59b77a1eee
jubayer-Cmd <sheikhabujubayer@gmail.com>
2023-12-19 17:18:34 +0600

    added notification

--- src/app/modules/notifications/notification.service.ts
+++ src/app/modules/notifications/notification.service.ts
@@ -2,7 +2,33 @@ import { PrismaClient, Notification } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const sendNotification = async (data: Notification): Promise<Notification> => {
+const sendNotification = async (data: Notification) => {
+  const { userId } = data; // Assuming 'userId' is part of the Notification object
+
+  // Check the number of notifications for the user
+  const userNotificationCount = await prisma.notification.count({
+    where: { userId },
+  });
+
+  if (userNotificationCount >= 20) {
+    const notificationsToDelete = await prisma.notification.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'asc' },
+      take: userNotificationCount - 19,
+    });
+
+    const notificationIdsToDelete = notificationsToDelete.map(
+      (notification) => notification.id,
+    );
+
+    await prisma.notification.deleteMany({
+      where: {
+        id: { in: notificationIdsToDelete },
+      },
+    });
+  }
+
+  // Create and return the new notification
   const newNotification = await prisma.notification.create({
     data,
   });
@@ -43,35 +69,11 @@ const deleteAllUserNotifications = async (userId: string): Promise<any> => {
   return 'Notifications deleted successfully';
 };
 
-const deleteExcessUserNotifications = async (userId: string): Promise<any> => {
-  const userNotificationCount = await prisma.notification.count({
-    where: { userId },
-  });
-
-  if (userNotificationCount > 20) {
-    const notificationsToDelete = await prisma.notification.findMany({
-      where: { userId },
-      orderBy: { createdAt: 'asc' },
-      take: userNotificationCount - 20,
-    });
-
-    const notificationIdsToDelete = notificationsToDelete.map(
-      (notification) => notification.id,
-    );
-
-    await prisma.notification.deleteMany({
-      where: {
-        id: { in: notificationIdsToDelete },
-      },
-    });
-  }
-  return 'Excess notifications deleted successfully';
-};
+//implement the corn job to delete the notifications after 24 hours
 
 export const notificationService = {
   sendNotification,
   getUserNotifications,
   deleteAllUserNotifications,
-  deleteExcessUserNotifications,
   sendNotificationToAllUsers,
 };
